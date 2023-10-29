3d61ea47d6f1d5f3c4bf2333e8b2053637cc712d
kiishi14 <aladesuru441@gmail.com>
2023-10-29 14:46:31 +0100

    components created

---
+++ app.js
@@ -0,0 +1,15 @@
+(() => {
+  let components = document.querySelectorAll("[data-include]");
+  const renderComponent = async (component) => {
+    try {
+      const { include } = component.dataset;
+      const componentPath = `./components/${include}.html`;
+      const getComponent = await fetch(componentPath);
+      const convertToHTML = await getComponent.text();
+      component.innerHTML = convertToHTML;
+    } catch (error) {
+      throw new Error("Failed to render component");
+    }
+  };
+  components.forEach(renderComponent);
+})();
